b1bd7dc6fc08386d9953fba3b2a3738f219f3929
Cam Moore <cammymoore07@gmail.com>
2024-08-17 09:24:12 -1000

    Done

--- src/components/StuffItem.tsx
+++ src/components/StuffItem.tsx
@@ -1,6 +1,15 @@
+'use client';
+
 import { Stuff } from '@prisma/client';
 import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { Trash } from 'react-bootstrap-icons';
+import { deleteStuff } from '@/lib/dbActions';
 
+const removeItem = (id: number) => {
+  console.log(`removeItem id: ${id}`);
+  deleteStuff(id);
+};
 /* Renders a single row in the List Stuff table. See list/page.tsx. */
 const StuffItem = ({ name, quantity, condition, id }: Stuff) => (
   <tr>
@@ -9,6 +18,11 @@ const StuffItem = ({ name, quantity, condition, id }: Stuff) => (
     <td>{condition}</td>
     <td>
       <Link href={`/edit/${id}`}>Edit</Link>
+    </td>
+    <td>
+      <Button variant="danger" onClick={() => removeItem(id)}>
+        <Trash />
+      </Button>
     </td>
   </tr>
 );
